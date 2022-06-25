ab0c1606d82799a85ee50d1994f8384407bb063c
Caitlin Swickard <caitlin6029@gmail.com>
2022-06-25 12:45:04 -0600

    set up express server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,20 @@
-// MERN = Mongo + Express + React + Node
 // Development = node.js server + react server
+
+
+// import and configure dotenv
+require('dotenv').config();
+
+// setting up express server
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+// port set up
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`listening on port ${port}`));
+
